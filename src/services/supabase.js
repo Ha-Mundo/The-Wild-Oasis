f3ef5ef185817376ddb1ae6aa4f439b8784f8f20
Ha-Mundo <trauzzu@gmail.com>
2024-06-24 14:46:39 +0100

    implemented supabase environment

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ylvhaxulvgqayjcxvveg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsdmhheHVsdmdxYXlqY3h2dmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyMjI0ODYsImV4cCI6MjAzNDc5ODQ4Nn0.U22p1GELqtbNBJNoBVgqJ1zh1dbKJeQkkzH1p0Ycs5M";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
